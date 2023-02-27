842840d88e8c20a971b4cda1cdb87219357100e1
Sergei <drigaserge@gmail.com>
2023-02-27 13:24:40 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+    }
+
+    public static void task1 () {
+        System.out.println("Task 5");
+    }
+
+    public static void task2 () {
+        System.out.println("\nTask 6");
+
+    }
+
+    public static void task3 () {
+        System.out.println("\nTask 7");
+    }
+
+    public static void task4 () {
+        System.out.println("\nTask 8");
+    }
+
+    public static void task5 () {
+        System.out.println("\nTask 9");
+    }
+
+    public static void task6 () {
+        System.out.println("\nTask 10");
+    }
+}
